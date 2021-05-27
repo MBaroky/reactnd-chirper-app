cf478b82f6effa66d8f9f3d331989e5ec8e93bcc
MBaroky <m.baroky@gmail.com>
2021-05-27 02:20:29 +0200

    tweet component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import  Tweet  from './Tweet'
 
 export class Dashboard extends Component {
     render() {
@@ -9,7 +10,9 @@ export class Dashboard extends Component {
                 <h3 className="center">Your Timeline</h3>
                 <ul className="dashboard-list">
                     {tweetIds.map(id=>
-                    (<li key= {id}> Tweet ID is: {id} </li>)
+                    (<li key= {id}>
+                        <Tweet id={id} />
+                    </li>)
                     )}
                 </ul>
             </div>
@@ -22,9 +25,6 @@ const mapStateToProps = ({ tweets }) => ({
     .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
 })
 
-const mapDispatchToProps = {
-
-}
 
 export default connect(mapStateToProps)(Dashboard)
 
